0caca799d4716a974018a4b9d042d86135e6f5bd
Mikkel Stårvik <mikkel.starvik@gmail.com>
2024-10-15 17:59:04 +0200

    Add desktop file and installation instructions

--- README.md
+++ README.md
@@ -14,5 +14,26 @@ A simple browser for UPnP-AV/DLNA resources on a local network.
 - Requires the preferred application to handle HTTP URLs
 - Does not support authentication
 
+## Installation
+### Dependencies
+- gtk4
+  - Arch: gtk4 (build/runtime)
+  - Ubuntu 24.04: libgtk-4-dev (build), libgtk-4-1 (runtime)
+- gupnp
+  - Arch: gupnp (build/runtime)
+  - Ubuntu 24.04: libgupnp-1.6-dev (build), libgupnp-1.6-0 (runtime)
+- gupnp-av
+  - Arch: gupnp-av (build/runtime)
+  - Ubuntu 24.04: libgupnp-av-1.0-dev (build), libgupnp-av-1.0-3 (runtime)
+
+### Building from source
+```sh
+git clone https://github.com/mstarvik/av-browser.git
+cd av-browser
+make
+sudo mv av-browser /usr/local/bin # optional
+sudo mv no.mstarvik.av-browser.desktop /usr/share/applications # optional
+```
+
 ## Troubleshooting
 If a resource fails to open, it may be due to a preferred application not being set for that resource type, or the preferred application not being able to handle HTTP URLs. Try running av-browser with the environment variable `G_MESSAGES_DEBUG=av-browser` to see the MIME type of the resource and the preferred application for that MIME type.
--- av-browser.c
+++ av-browser.c
@@ -171,26 +171,26 @@ static void browse(Browser *browser, GUPnPServiceProxy *content_directory, const
     //       media servers list on error.
 }
 
-static void object_available_cb(GUPnPDIDLLiteParser *parser, GUPnPDIDLLiteObject *object, Browser *browser_data) {
+static void object_available_cb(GUPnPDIDLLiteParser *parser, GUPnPDIDLLiteObject *object, Browser *browser) {
     const char *parent_id = gupnp_didl_lite_object_get_parent_id(object);
     const char *object_id = gupnp_didl_lite_object_get_id(object);
 
-    if (g_strcmp0(object_id, browser_data->current_item->object_id) == 0) {
+    if (g_strcmp0(object_id, browser->current_item->object_id) == 0) {
         // The object is the current item, update its metadata
 
         item_set_metadata(
-            browser_data->current_item,
+            browser->current_item,
             parent_id,
             gupnp_didl_lite_object_get_title(object),
             gupnp_didl_lite_object_get_upnp_class(object)
         );
 
-        gtk_window_set_title(GTK_WINDOW(browser_data->window), browser_data->current_item->title);
-        gtk_widget_set_sensitive(browser_data->up_button, TRUE);
-    } else if (g_strcmp0(parent_id, browser_data->current_item->object_id) == 0) {
+        gtk_window_set_title(GTK_WINDOW(browser->window), browser->current_item->title);
+        gtk_widget_set_sensitive(browser->up_button, TRUE);
+    } else if (g_strcmp0(parent_id, browser->current_item->object_id) == 0) {
         // The object is a child of the current item, add it to the list
 
-        Item *child = item_new(browser_data->current_item->content_directory, object_id);
+        Item *child = item_new(browser->current_item->content_directory, object_id);
         item_set_metadata(
             child,
             parent_id,
@@ -212,13 +212,13 @@ static void object_available_cb(GUPnPDIDLLiteParser *parser, GUPnPDIDLLiteObject
             g_list_free_full(resources, g_object_unref);
         }
 
-        g_list_store_append(browser_data->items, child);
+        g_list_store_append(browser->items, child);
         g_object_unref(child);
     }
 }
 
-static void device_proxy_available_cb(GUPnPControlPoint *cp, GUPnPDeviceProxy *proxy, Browser *browser_data) {
-    if (browser_data->current_item->content_directory == NULL) {
+static void device_proxy_available_cb(GUPnPControlPoint *cp, GUPnPDeviceProxy *proxy, Browser *browser) {
+    if (browser->current_item->content_directory == NULL) {
         // Not currently browsing a media server, show a list of media servers
 
         GUPnPServiceInfo *content_directory = gupnp_device_info_get_service(GUPNP_DEVICE_INFO(proxy), "urn:schemas-upnp-org:service:ContentDirectory:1");
@@ -230,13 +230,13 @@ static void device_proxy_available_cb(GUPnPControlPoint *cp, GUPnPDeviceProxy *p
                 gupnp_device_info_get_friendly_name(GUPNP_DEVICE_INFO(proxy)),
                 "object.container"
             );
-            g_list_store_append(browser_data->items, item);
+            g_list_store_append(browser->items, item);
             g_object_unref(content_directory);
         }
     }
 }
 
-static void setup_item_cb(GtkSignalListItemFactory *factory, GtkListItem *list_item, Browser *browser_data) {
+static void setup_item_cb(GtkSignalListItemFactory *factory, GtkListItem *list_item, Browser *browser) {
     GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_list_item_set_child(list_item, box);
 
@@ -251,12 +251,12 @@ static void setup_item_cb(GtkSignalListItemFactory *factory, GtkListItem *list_i
     gtk_box_append(GTK_BOX(box), label);
 }
 
-static void bind_item_cb(GtkSignalListItemFactory *factory, GtkListItem *list_item, Browser *browser_data) {
+static void bind_item_cb(GtkSignalListItemFactory *factory, GtkListItem *list_item, Browser *browser) {
     GtkWidget *box = gtk_list_item_get_child(list_item);
     Item *item = gtk_list_item_get_item(list_item);
     
     GtkWidget *icon = gtk_widget_get_first_child(box);
-    if (browser_data->current_item->content_directory) {
+    if (browser->current_item->content_directory) {
         gtk_image_set_from_icon_name(GTK_IMAGE(icon), upnp_class_to_icon(item->upnp_class));
     } else {
         gtk_image_set_from_icon_name(GTK_IMAGE(icon), "folder-remote");
@@ -266,11 +266,11 @@ static void bind_item_cb(GtkSignalListItemFactory *factory, GtkListItem *list_it
     gtk_label_set_text(GTK_LABEL(label), item->title);
 }
 
-static void activate_item_cb(GtkGridView *grid_view, guint position, Browser *browser_data) {
-    Item *item = g_list_model_get_item(G_LIST_MODEL(browser_data->items), position);
+static void activate_item_cb(GtkGridView *grid_view, guint position, Browser *browser) {
+    Item *item = g_list_model_get_item(G_LIST_MODEL(browser->items), position);
 
     if (g_str_has_prefix(item->upnp_class, "object.container")) {
-        browse(browser_data, item->content_directory, item->object_id);
+        browse(browser, item->content_directory, item->object_id);
     } else if (g_str_has_prefix(item->upnp_class, "object.item")) {
         GAppInfo *app_info = g_app_info_get_default_for_type(item->resource_type, TRUE);
         if (app_info) {
@@ -284,61 +284,61 @@ static void activate_item_cb(GtkGridView *grid_view, guint position, Browser *br
     }
 }
 
-static void up_button_clicked_cb(GtkButton *button, Browser *browser_data) {
-    if (browser_data->current_item->parent_id) {
-        if (g_strcmp0(browser_data->current_item->parent_id, "-1") == 0) {
+static void up_button_clicked_cb(GtkButton *button, Browser *browser) {
+    if (browser->current_item->parent_id) {
+        if (g_strcmp0(browser->current_item->parent_id, "-1") == 0) {
             // The current item is the root, go back to media servers list
 
-            g_object_unref(browser_data->current_item);
-            browser_data->current_item = item_new(NULL, "-1");
-            g_list_store_remove_all(browser_data->items);
+            g_object_unref(browser->current_item);
+            browser->current_item = item_new(NULL, "-1");
+            g_list_store_remove_all(browser->items);
 
-            gssdp_resource_browser_set_active(GSSDP_RESOURCE_BROWSER(browser_data->cp), FALSE);
-            gssdp_resource_browser_set_active(GSSDP_RESOURCE_BROWSER(browser_data->cp), TRUE);
+            gssdp_resource_browser_set_active(GSSDP_RESOURCE_BROWSER(browser->cp), FALSE);
+            gssdp_resource_browser_set_active(GSSDP_RESOURCE_BROWSER(browser->cp), TRUE);
 
-            gtk_window_set_title(GTK_WINDOW(browser_data->window), "Media Servers");
-            gtk_widget_set_sensitive(browser_data->up_button, FALSE);
+            gtk_window_set_title(GTK_WINDOW(browser->window), "Media Servers");
+            gtk_widget_set_sensitive(browser->up_button, FALSE);
         } else {
             // browse replaces the current item, so we need to keep a reference to it
-            Item *old_current = g_object_ref(browser_data->current_item);
+            Item *old_current = g_object_ref(browser->current_item);
 
-            browse(browser_data, old_current->content_directory, old_current->parent_id);
+            browse(browser, old_current->content_directory, old_current->parent_id);
 
             g_object_unref(old_current);
         }
     }
 }
 
-static void activate_cb(GtkApplication *app, Browser *browser_data) {
+static void activate_cb(GtkApplication *app, Browser *browser) {
     GError *error = NULL;
 
-    browser_data->window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(browser_data->window), "Media Servers");
-    gtk_window_set_default_size(GTK_WINDOW(browser_data->window), 800, 600);
+    browser->window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(browser->window), "Media Servers");
+    gtk_window_set_default_size(GTK_WINDOW(browser->window), 800, 600);
 
     GtkWidget *header_bar = gtk_header_bar_new();
     gtk_header_bar_set_show_title_buttons(GTK_HEADER_BAR(header_bar), TRUE);
-    gtk_window_set_titlebar(GTK_WINDOW(browser_data->window), header_bar);
+    gtk_window_set_titlebar(GTK_WINDOW(browser->window), header_bar);
 
-    browser_data->up_button = gtk_button_new_from_icon_name("go-up-symbolic");
-    gtk_widget_set_sensitive(browser_data->up_button, FALSE);
-    g_signal_connect(browser_data->up_button, "clicked", G_CALLBACK(up_button_clicked_cb), browser_data);
-    gtk_header_bar_pack_start(GTK_HEADER_BAR(header_bar), browser_data->up_button);
+    browser->up_button = gtk_button_new_from_icon_name("go-up-symbolic");
+    gtk_widget_set_sensitive(browser->up_button, FALSE);
+    g_signal_connect(browser->up_button, "clicked", G_CALLBACK(up_button_clicked_cb), browser);
+    gtk_header_bar_pack_start(GTK_HEADER_BAR(header_bar), browser->up_button);
 
     GtkWidget *scrolled_window = gtk_scrolled_window_new();
     gtk_widget_set_vexpand(scrolled_window, TRUE);
     gtk_widget_set_hexpand(scrolled_window, TRUE);
-    gtk_window_set_child(GTK_WINDOW(browser_data->window), scrolled_window);
+    gtk_window_set_child(GTK_WINDOW(browser->window), scrolled_window);
 
-    browser_data->items = g_list_store_new(ITEM_TYPE);
-    GtkSingleSelection *model = gtk_single_selection_new(G_LIST_MODEL(browser_data->items));
+    browser->items = g_list_store_new(ITEM_TYPE);
+    GtkSingleSelection *model = gtk_single_selection_new(G_LIST_MODEL(browser->items));
 
     GtkListItemFactory *factory = gtk_signal_list_item_factory_new();
-    g_signal_connect(factory, "setup", G_CALLBACK(setup_item_cb), browser_data);
-    g_signal_connect(factory, "bind", G_CALLBACK(bind_item_cb), browser_data);
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_item_cb), browser);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_item_cb), browser);
 
     GtkWidget *grid_view = gtk_grid_view_new(GTK_SELECTION_MODEL(model), factory);
-    g_signal_connect(grid_view, "activate", G_CALLBACK(activate_item_cb), browser_data);
+    g_signal_connect(grid_view, "activate", G_CALLBACK(activate_item_cb), browser);
     gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), grid_view);
 
     GtkCssProvider *css_provider = gtk_css_provider_new();
@@ -368,23 +368,23 @@ static void activate_cb(GtkApplication *app, Browser *browser_data) {
         return;
     }
 
-    browser_data->parser = gupnp_didl_lite_parser_new();
-    g_signal_connect(browser_data->parser, "object-available", G_CALLBACK(object_available_cb), browser_data);
+    browser->parser = gupnp_didl_lite_parser_new();
+    g_signal_connect(browser->parser, "object-available", G_CALLBACK(object_available_cb), browser);
 
-    browser_data->cp = gupnp_control_point_new(context, "urn:schemas-upnp-org:device:MediaServer:1");
-    g_signal_connect(browser_data->cp, "device-proxy-available", G_CALLBACK(device_proxy_available_cb), browser_data);
-    gssdp_resource_browser_set_active(GSSDP_RESOURCE_BROWSER(browser_data->cp), TRUE);
+    browser->cp = gupnp_control_point_new(context, "urn:schemas-upnp-org:device:MediaServer:1");
+    g_signal_connect(browser->cp, "device-proxy-available", G_CALLBACK(device_proxy_available_cb), browser);
+    gssdp_resource_browser_set_active(GSSDP_RESOURCE_BROWSER(browser->cp), TRUE);
 
-    gtk_window_present(GTK_WINDOW(browser_data->window));
+    gtk_window_present(GTK_WINDOW(browser->window));
 }
 
 int main(int argc, char **argv) {
-    Browser browser_data = {
+    Browser browser = {
         .current_item = item_new(NULL, "-1")
     };
 
     GtkApplication *app = gtk_application_new("no.mstarvik.av-browser", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(activate_cb), &browser_data);
+    g_signal_connect(app, "activate", G_CALLBACK(activate_cb), &browser);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
     return status;
